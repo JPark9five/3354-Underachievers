e35e5de58346c6429196591e57f44e64904c2823
JPark9five <31904917+JPark9five@users.noreply.github.com>
2017-12-06 23:48:11 -0600

    Create Test1.java
    Moving James' test case from root directory to test case folder

---
+++ SMSApplication/app/src/androidTest/java/com/example/briantruong/smsapplication/Test1.java
@@ -0,0 +1,39 @@
+package com.example.briantruong.smsapplication;
+
+import org.junit.Test;
+import org.junit.Before;
+import android.widget.EditText;
+import static org.junit.Assert.*;
+
+/**
+ * Created by jande on 12/6/2017.
+ */
+
+public class Test1 extends AddContact{
+
+    public EditText startName=(EditText) findViewById(R.id.nameInput);
+    @Before
+    public void setUp() throws Exception{//set up places joe in the Name text
+        //super.setUp();
+
+        //EditText startName=(EditText) findViewById(R.id.nameInput);
+
+        startName.setText("Joe");
+
+    }
+    @Test
+    public void testAddContact(){
+
+        AddContact test= new AddContact();
+        //EditText startName=(EditText) findViewById(R.id.nameInput); that erro
+
+        //test.startName;
+        assertNotNull(test.getEditName());
+//failing to assertNotNull and assertNull
+    }
+    public EditText getEditName(){
+        return this.startName;
+    }
+
+
+}
